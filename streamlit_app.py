b4736911a9300e8ae7fbc2f228fc7d68bb05e774
NataliaMCatalan <108061043+NataliaMCatalan@users.noreply.github.com>
2023-04-03 17:07:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.subtitle('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal/tKale, Spinach & Rocket/tSmoothie/tHard-Boiled Free-Range Egg')
+
