b8de962bb32929d67eb88b4d67be6973b6babfaf
NataliaMCatalan <108061043+NataliaMCatalan@users.noreply.github.com>
2023-04-04 08:33:56 +0200

    Update streamlit_app.py
    Habilitamos la opción de que se puedan seleccionar las frutas que se quieran

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+#Let´s put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(My_fruit_list.index), ['Advocado', 'Strewberries']
